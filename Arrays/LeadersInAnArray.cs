ebb5279d7748b345ffd8e9f9d0bd630eaaeaa30b
vivek kumar <vivk@microsoft.com>
2022-07-17 23:52:01 +0530

    LeadersInAnArray

--- Arrays/LeadersInAnArray.cs
+++ Arrays/LeadersInAnArray.cs
@@ -7,17 +7,14 @@ using System.Threading.Tasks;
 namespace DSA.Arrays
 {
     //Leaders in an array
-    //    Given an array A of positive integers.Your task is to find the leaders in the array.An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader.
-
-
+    //    Given an array A of positive integers.Your task is to find the leaders in the array.An element of array is leader
+    //    if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader.
 
     //Example 1:
-
-
     //Input:
     //n = 6
     //A[] = { 16,17,4,3,5,2}
-    //    Output: 17 5 2
+    //Output: 17 5 2
     //Explanation: The first leader is 17 
     //as it is greater than all the elements
     //to its right.Similarly, the next
@@ -32,13 +29,12 @@ namespace DSA.Arrays
     //Input:
     //n = 5
     //A[] = { 1,2,3,4,0}
-    //    Output: 4 0
+    //Output: 4 0
 
 
     //Your Task:
-    //You don't need to read input or print anything. The task is to complete the function leader() which takes array A and n as input parameters and returns an array of leaders in order of their appearance.
-
-
+    //You don't need to read input or print anything. The task is to complete the function leader() which takes array A and n as input parameters and
+    //returns an array of leaders in order of their appearance.
 
     //Expected Time Complexity: O(n)
     //Expected Auxiliary Space: O(n)
@@ -46,19 +42,23 @@ namespace DSA.Arrays
     {
         public int[] Leaders(int[] arr, int n)
         {
+            //A[] = { 16,17,4,3,5,2}
+            //Output: 17 5 2
             int max = arr[n - 1];
-            List<int> leadersArr = new List<int>();
+            List<int> leaders = new List<int>();
+            leaders.Insert(0, arr[n - 1]);
 
-            for (int i = n - 1; i >= 0; i--)
+            for (int i = n - 2; i >= 0; i--)
             {
-                if (max <= arr[i])
+                if (arr[i] >= max)
                 {
                     max = arr[i];
-                    leadersArr.Insert(0, max);
+                    leaders.Insert(0, arr[i]);
                 }
+
             }
 
-            return leadersArr.ToArray();
+            return leaders.ToArray();
         }
     }
 }
