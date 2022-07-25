ec9e6e0cd0fa6f641be86cc7e7eed01b34347ba7
vivek kumar <vivk@microsoft.com>
2022-07-26 00:38:01 +0530

    MajorityElement GetContainsDuplicate

---
+++ Arrays/ContainsDuplicate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    public static class ContainsDuplicate
+    {
+        //Contains Duplicate
+        //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+        //Example 1:
+        //Input: nums = [1,2,3,1]
+        //Output: true
+
+        //Example 2:
+        //Input: nums = [1,2,3,4]
+        //Output: false
+
+        //Example 3:
+        //Input: nums = [1,1,1,3,3,4,3,2,4,2]
+        //Output: true
+        public static bool GetContainsDuplicate(int[] nums)
+        {
+            //Input: nums = [1, 2, 3, 1]
+            //Output: true
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+
+                dict.Add(nums[i], 1);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Arrays/MajorityElement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Arrays
+{
+    public static class MajorityElement
+    {
+        //Given an array nums of size n, return the majority element.
+        //The majority element is the element that appears more than ⌊n / 2⌋ times.You may assume that the majority element always exists in the array.
+
+        //Example 1:
+        //Input: nums = [3, 2, 3]
+        //Output: 3
+
+        //Example 2:
+        //Input: nums = [2, 2, 1, 1, 1, 2, 2]
+        //Output: 2
+
+        public static int GetMajorityElement(int[] nums)
+        {
+            //Input: nums = [2, 2, 1, 1, 1, 2, 2]
+            //Output: 2
+
+            //Input: [2,2,1,1, 1, 2, 2]
+            // Output:2
+            int count = 0;//1,2,1,0,1,0,1
+            int majorityValue = 0;//2,2,2,2,1,1,2           
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count == 0)
+                {
+                    majorityValue = nums[i];
+                }
+
+                if (majorityValue == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+
+            return majorityValue;
+        }
+    }
+}
