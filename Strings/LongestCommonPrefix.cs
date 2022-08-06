a8d420f11c25820db64bcf08961f8e55e0df93a5
Vivek Kumar <vivek1273@gmail.com>
2022-08-07 00:37:24 +0530

    LongestCommonPrefix

---
+++ Strings/LongestCommonPrefix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Strings
+{
+    public class LongestCommonPrefix
+    {
+        //Longest Common Prefix
+        //Write a function to find the longest common prefix string amongst an array of strings.
+        //If there is no common prefix, return an empty string "".
+
+        //Example 1:
+        //Input: strs = ["flower", "flow", "flight"]
+        //Output: "fl"
+
+        //Example 2:
+        //Input: strs = ["dog", "racecar", "car"]
+        //Output: ""
+        //Explanation: There is no common prefix among the input strings.
+
+        public string GetLongestCommonPrefix(string[] strs)
+        {
+            string res = "";
+            int minLength = strs[0].Length;
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                minLength = Math.Min(minLength, strs[i].Length);
+            }
+
+            //Input: strs = ["flower", "flow", "flight"]
+            //Output: "fl"
+            bool flag = true;
+            char ch = ' ';
+            for (int i = 0; i < minLength; i++)
+            {
+                ch = strs[0][i];
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (ch != strs[j][i])
+                    {
+                        return res;
+                    }
+                }
+
+                res += ch;
+            }
+
+            return res;
+
+        }
+    }
+}
---
+++ Strings/LongestPalindromicSubstring.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Strings
+{
+    public class LongestPalindromicSubstring
+    {
+        //Longest Palindromic Substring
+        //Given a string s, return the longest palindromic substring in s.
+
+        //Example 1:
+        //Input: s = "babad"
+        //Output: "bab"
+        //Explanation: "aba" is also a valid answer.
+
+        //Example 2:
+        //Input: s = "cbbd"
+        //Output: "bb"
+
+        public string LongestPalindrome(string s)
+        {
+            return "";
+        }
+    }
+}
